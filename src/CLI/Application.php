4df9aad6f389c841ba687cff9f25453cf05e016f
Sebastian Bergmann <sb@sebastian-bergmann.de>
2014-06-27 13:36:44 +0200

    Prepare release

--- src/CLI/Application.php
+++ src/CLI/Application.php
@@ -63,7 +63,7 @@ class Application extends AbstractApplication
 {
     public function __construct()
     {
-        $version = new Version('1.0', dirname(dirname(__DIR__)));
+        $version = new Version('1.0.0', dirname(dirname(__DIR__)));
         parent::__construct('de-legacy-fy', $version->getVersion());
 
         $this->add(new GenerateCharacterizationTestCommand);
